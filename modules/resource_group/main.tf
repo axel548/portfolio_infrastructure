312560a7fe217d0059a39a7ce4819775d5c13e18
Axel Lopez <axel.aatl@gmail.com>
2025-10-21 13:53:38 -0600

    [DELETE] resource group file to use module

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = "rg-" + var.rg_name
+  name     = var.rg_name
   location = var.location
 }
--- resource_group.tf
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------------------
-# Resource Group
-# --------------------------------------------------------
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-" + var.resource_group_name
-  location = var.location
-}
